9a08d2a21641a0344bae92b89411a76c5159550e
Pranjal Sharma <127693503+code-prsh@users.noreply.github.com>
2024-05-11 16:52:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MovieSearch from './components/Homepage';
 import MovieDetails from './components/MovieDetails';
 
